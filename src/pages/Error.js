3a24a7771feeed19fd834734cb32c041a66a6101
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-30 10:23:24 +1300

    added an error Page

---
+++ src/pages/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+	return (
+		<div>
+			<h2>404</h2>
+			<p>page not found!</p>
+			<button className='btn'>Back to Home</button>
+		</div>
+	);
+};
+
+export default Error;
