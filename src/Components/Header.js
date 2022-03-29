c5ed59aac48be7956daa724ddce882be39bf61d1
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-29 23:02:53 +1300

    added a components folder and pages folder

---
+++ src/Components/Header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import imageHeroDesktop from '../assets/desktop/image-hero.jpg';
+import imageHeroDesktop2x from '../assets/desktop/image-hero@2x.jpg';
+import imageHero from '../assets/mobile/image-hero.jpg';
+import imageHeroMobile2x from '../assets/mobile/image-hero@2x.jpg';
+import imageHeroTablet2x from '../assets/tablet/image-hero@2x.jpg';
+import imageHeroTablet from '../assets/tablet/image-hero.jpg';
+import { FaChevronRight } from 'react-icons/fa';
+
+const Header = () => {
+	return (
+		<header>
+			<div className='hero-image'>
+				<picture>
+					<source
+						srcset={imageHero}
+						media='(max-width: 375px)'
+						size={imageHeroMobile2x}
+					/>
+					<source
+						srcset={imageHeroTablet}
+						media='(max-width: 768px)'
+						size={imageHeroTablet2x}
+					/>
+					<source
+						srcset={imageHeroDesktop}
+						media='(max-width: 1440px)'
+						size={imageHeroDesktop2x}
+					/>
+					<img src={imageHeroDesktop} alt='hero' className='img' />
+				</picture>
+			</div>
+			<div className='hero-text'>
+				<h1> Modern Art Gallery</h1>
+				<p>
+					The arts in the collection of the Modern Art Gallery all started from
+					a spark of inspiration. Will these pieces inspire you? Visit us and
+					find out.
+				</p>
+				<button className='btn'>
+					Our location <span>{<FaChevronRight />}</span>
+				</button>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main = () => {
+	return (
+		<main className='main'>
+			<section className='section'>section1</section>
+			<section className='section'>Section2</section>
+			<section className='section'>Section3</section>
+		</main>
+	);
+};
+
+export default Main;
