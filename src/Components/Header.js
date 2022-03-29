735c293c16377996d24cfad699bca884f8b540f8
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-30 12:04:42 +1300

    set up the Hero image for different screens

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import imageHeroMobile from '../assets/mobile/image-hero.jpg';
+import imageHeroTablet from '../assets/tablet/image-hero.jpg';
 import imageHeroDesktop from '../assets/desktop/image-hero.jpg';
-import imageHeroDesktop2x from '../assets/desktop/image-hero@2x.jpg';
-import imageHero from '../assets/mobile/image-hero.jpg';
 import imageHeroMobile2x from '../assets/mobile/image-hero@2x.jpg';
 import imageHeroTablet2x from '../assets/tablet/image-hero@2x.jpg';
-import imageHeroTablet from '../assets/tablet/image-hero.jpg';
+import imageHeroDesktop2x from '../assets/desktop/image-hero@2x.jpg';
 import { FaChevronRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
@@ -14,21 +14,21 @@ const Header = () => {
 			<div className='hero-image'>
 				<picture>
 					<source
-						srcset={imageHero}
-						media='(max-width: 375px)'
+						srcset={imageHeroMobile}
+						media='(min-width: 375px)'
 						size={imageHeroMobile2x}
 					/>
 					<source
 						srcset={imageHeroTablet}
-						media='(max-width: 768px)'
+						media='(min-width: 768px)'
 						size={imageHeroTablet2x}
 					/>
 					<source
 						srcset={imageHeroDesktop}
-						media='(max-width: 1440px)'
+						media='(min-width: 1440px)'
 						size={imageHeroDesktop2x}
 					/>
-					<img src={imageHeroDesktop} alt='hero' className='img' />
+					<img src={imageHeroMobile} alt='hero' className='img' />
 				</picture>
 			</div>
 			<div className='hero-text'>
