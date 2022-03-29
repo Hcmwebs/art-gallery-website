45a08446d993017e2cee50ce432e88e460f0cbcf
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-29 23:19:25 +1300

    added a footer component and deleted the location.html

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Footer = () => {
-	return <footer className='footer'>Footer goes here</footer>;
+	return (
+		<footer className='footer'>
+			<h3> Modern Art Gallery</h3>
+			<p>
+				The Modern Art Gallery is free to all visitors and open seven days a
+				week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
+			</p>
+			<div className='social-media'>
+				<FaFacebookSquare />
+				<FaInstagram />
+				<FaTwitter />
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,44 +1,39 @@
 import React from 'react';
-import { FaChevronLeft,FaLocationArrow,FaTwitter, FaInstagram,FaFacebookSquare} from 'react-icons/fa';
+import {
+	FaChevronLeft,
+	FaLocationArrow,
+	FaTwitter,
+	FaInstagram,
+	FaFacebookSquare,
+} from 'react-icons/fa';
+import Footer from '../Components/Footer';
 const Location = () => {
 	return (
-		<main className='main'>
-			<button className='btn'>
-				<span>{<FaChevronLeft />}</span>Back to Home
-			</button>
-			<section className='section'>
-				<h2>Our location</h2>
-				<address>
+		<>
+			<main className='main'>
+				<button className='btn'>
+					<span>{<FaChevronLeft />}</span>Back to Home
+				</button>
+				<section className='section'>
+					<h2>Our location</h2>
+					<address>
+						<p>
+							<span className='address-header'>99 King Street</span> <br />
+							Newport <br />
+							RI 02840 <br />
+							United States of America
+						</p>
+					</address>
+
 					<p>
-						<span className='address-header'>99 King Street</span> <br />
-						Newport <br />
-						RI 02840 <br />
-						United States of America
+						Our newly opened gallery is located near the Edward King House on 99
+						King Street, the Modern Art Gallery is free to all visitors and open
+						seven days a week from 8am to 9pm.
 					</p>
-				</address>
-
-				<p>
-					Our newly opened gallery is located near the Edward King House on 99
-					King Street, the Modern Art Gallery is free to all visitors and open
-					seven days a week from 8am to 9pm.
-				</p>
-			</section>
-			<section className='section'>
-				<h3> Modern Art Gallery</h3>
-				<p>
-					The Modern Art Gallery is free to all visitors and open seven days a
-					week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
-				</p>
-				<div className="social-media">
-
-				</div>
-			</section>
-			<footer className='footer'>
-				<FaFacebookSquare />
-				<FaInstagram />
-				<FaTwitter />
-			</footer>
-		</main>
+				</section>
+			</main>
+			<Footer />
+		</>
 	);
 };
 
