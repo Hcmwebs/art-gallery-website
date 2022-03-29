fc8cb93c41e2cb628c9bacf0cadcc5f1bbf1d8de
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-30 10:32:29 +1300

    Linked the page btn back home

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => {
 	return (
 		<div>
 			<h2>404</h2>
 			<p>page not found!</p>
-			<button className='btn'>Back to Home</button>
+			<Link to='/' className='btn'>
+				Back to Home
+			</Link>
 		</div>
 	);
 };
