d9c8ea14e22d43853604bc052cd214ab0c1f2eaa
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-30 10:24:29 +1300

    Added the router dom for navigation around the pages
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Home from './pages/Home';
 import Location from './pages/Location';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Error from './pages/Error';
 
 const App = () => {
 	return (
-		<div className='container'>
-			<Home />
-			<Location />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='location' element={<Location />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+		</BrowserRouter>
 	);
 };
 
