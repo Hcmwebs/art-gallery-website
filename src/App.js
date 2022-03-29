8f47fe6c9ad5db469d8ae3a4111663817ace4421
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-30 00:29:27 +1300

    modified the Main with all the markup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
 import Home from './pages/Home';
 import Location from './pages/Location';
 
-function App() {
-  return (
-    <div className="container">
-      <Home />
-      <Location />
-
-    </div>
-  )
-}
+const App = () => {
+	return (
+		<div className='container'>
+			<Home />
+			<Location />
+		</div>
+	);
+};
 
 export default App;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,9 +3,35 @@ import React from 'react';
 const Main = () => {
 	return (
 		<main className='main'>
-			<section className='section'>section1</section>
-			<section className='section'>Section2</section>
-			<section className='section'>Section3</section>
+			<section className='section'>
+				<div className='section-image'>
+					<img src='' alt='section image' className='img' />
+				</div>
+				<div className='section-text'>
+					<h2>Your day at the gallery</h2>
+					<p>
+						Wander through our distinct collections and find new insights about
+						our artists. Dive into the details of their creative process.
+					</p>
+				</div>
+			</section>
+			<section className='section'>
+				<div className='section-image'>
+					<img src='' alt='' className='img' />
+				</div>
+				<div className='section-grid'>
+					<div className='section-image'>
+						<img src='' alt='' className='img' />
+					</div>
+					<div className='section-text'>
+						<h3> Come &amp; be inspired</h3>
+						<p>
+							We’re excited to welcome you to our gallery and see how our
+							collections influence you.
+						</p>
+					</div>
+				</div>
+			</section>
 		</main>
 	);
 };
