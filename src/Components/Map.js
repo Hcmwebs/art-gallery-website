86b9429985c4d19ef801463fc3fc66a2a0bcf2cf
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-31 22:25:04 +1300

    added address to the map pop up

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -3,15 +3,24 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Map = () => {
 	return (
-		<section className='section' styles={{ minHeight: '30vh' }}>
-			<MapContainer center={[51.505, -0.09]} zoom={13}>
+		<section className='section leaflet-container'>
+			<MapContainer
+				center={[41.481485, -71.310328]}
+				zoom={12}
+				scrollWheelZoom={true}>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 				/>
-				<Marker position={[51.505, -0.09]}>
+				<Marker position={[41.481485, -71.310328]}>
 					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
+						<h3>Modern Art Gallery</h3>
+						<address>
+							99 King Street <br />
+							Newport <br />
+							RI 02840 <br />
+							United States of America
+						</address>
 					</Popup>
 				</Marker>
 			</MapContainer>
