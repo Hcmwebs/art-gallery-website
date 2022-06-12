e93d99b71a28b1470c5029d99dc796e675affa03
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-12 15:39:15 +1200

    refactored and styled the footer code

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,15 +4,14 @@ import { FaTwitter, FaInstagram, FaFacebookSquare } from 'react-icons/fa'
 const Footer = () => {
 	return (
 		<footer className='footer footer-home'>
-			<div className='footer-content'>
-				<h5>
-					Modern <br /> Art Gallery
-				</h5>
-				<p>
-					The Modern Art Gallery is free to all visitors and open seven days a
-					week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
-				</p>
-			</div>
+			<h3>
+				Modern <br /> Art Gallery
+			</h3>
+			<p>
+				The Modern Art Gallery is free to all visitors and open seven days a
+				week from 8am to 9pm. Find us at 99 King Street, Newport, USA.
+			</p>
+
 			<div className='footer-social-media'>
 				<a
 					href='https://facebook.com/'
