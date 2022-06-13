02b9e8cc2dc0d3e6be82c9d6b9869d67f1c73c42
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-14 07:43:06 +1200

    removed unnessary Error Js component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react'
 import Home from './pages/Home'
 import Location from './pages/Location'
 import { Routes, Route } from 'react-router-dom'
-import Error from './pages/Error'
+
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
 			<Route path='location' element={<Location />} />
-			<Route path='*' element={<Error />} />
 		</Routes>
 	)
 }
